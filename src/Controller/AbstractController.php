adba625b078fd73fdb8b0af106476461a8d20c35
mave <plihal.marek@seznam.cz>
2025-01-19 18:28:26 +0100

    safsdfsd

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,26 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as AbstractControllerBase;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 class AbstractController extends AbstractControllerBase
 {
 
     protected EntityManagerInterface $entityManager;
+    protected RequestStack $requestStack;
 
     public function __construct(
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        RequestStack $requestStack
     ) {
         $this->entityManager = $entityManager;
+        $this->requestStack = $requestStack;
         date_default_timezone_set("Europe/Prague");
     }
 
     protected function getGlobalParameters(): array
     {
-        return [];
+        return [
+            'cart' => $this->getDisplayCart()
+        ];
     }
 
     protected function renderView(string $view, array $parameters = []): string
@@ -42,5 +49,45 @@ class AbstractController extends AbstractControllerBase
     ): RedirectResponse {
         $this->addFlash($type, $message);
         return $this->redirectToRoute($route, $parameters);
+    }
+
+    protected function getCart() : array
+    {
+        $session = $this->requestStack->getSession();
+
+        return $session->get('cart', [
+            'products' => [],
+            'total' => 0,
+        ]);
+    }
+
+    protected function saveCart(array $cart) : void
+    {
+        $session = $this->requestStack->getSession();
+
+        $session->set('cart', $cart);
+    }
+
+    protected function getDisplayCart() : array
+    {
+        $cart = $this->getCart();
+
+        $productIds = array_keys($cart['products']);
+
+        $products = $this->entityManager->getRepository(Product::class)->findBy(['id' => $productIds]);
+
+        $displayableProducts = [];
+        foreach ($products as $product) {
+            $productId = $product->getId();
+            $displayableProducts[] = [
+                'product' => $product, // The Doctrine entity
+                'quantity' => $productsArray['products'][$productId] ?? 0,
+            ];
+        }
+
+        return [
+            'products' => $displayableProducts,
+            'total' => $cart['total'],
+        ];
     }
 }
--- src/Controller/ShoppingCartController.php
+++ src/Controller/ShoppingCartController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ShoppingCartController extends AbstractController
 {
@@ -13,4 +15,52 @@ class ShoppingCartController extends AbstractController
         return $this->render('shoppingCart.html.twig', [
         ]);
     }
+
+    #[Route("/cart/add/{id}", name: "add_cart")]
+    public function menu(string $id): Response
+    {
+        $cart = $this->getCart();
+
+        $product = $this->entityManager->find(Product::class, $id);
+
+        if ($product === null) {
+            return $this->flashRedirect('error', 'Produkt nenalezen', 'main');
+        }
+
+        if (isset($cart['products'][$id])) {
+            $cart['products'][$id]++;
+        } else {
+            $cart['products'][$id] = 1;
+        }
+
+        $this->saveCart($cart);
+
+        return $this->redirectToRoute('tag', ['id' => $product->getTag()->getId()]);
+    }
+
+    #[Route("/cart/remove/{id}", name: "remove_cart")]
+    public function removeTag(string $id): Response
+    {
+        $cart = $this->getCart();
+
+        $product = $this->entityManager->find(Product::class, $id);
+
+        if ($product === null) {
+            return $this->flashRedirect('error', 'Produkt nenalezen', 'main');
+        }
+
+        if (isset($cart['products'][$id])) {
+
+            $cart['products'][$id]--;
+
+            if ($cart['products'][$id] == 0) {
+                unset($cart['products'][$id]);
+            }
+
+            $this->saveCart($cart);
+        }
+
+        return $this->redirectToRoute('tag', ['id' => $product->getTag()->getId()]);
+    }
+
 }
