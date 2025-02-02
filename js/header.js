64e74895df252cbbf5c66a70a54bbcc8486d4c04
mave <plihal.marek@seznam.cz>
2025-02-02 20:53:46 +0100

    sadas

--- js/header.js
+++ js/header.js
@@ -1,27 +1,19 @@
-$(document).ready(function () {
-    // Kliknutí na #iconAndNameDiv zobrazí nebo skryje submenu
-    $("#iconAndNameDiv").click(function (event) {
-        event.stopPropagation(); // Zastaví propagaci události kliknutí
-        $("#submenu").toggle(); // Přepne viditelnost submenu
-        if ($("#submenu").css("display") == "block") {
-            $("#figureIcon").addClass("figureIconOn");
-        }
-        if ($("#submenu").css("display") == "none") {
-            $("#figureIcon").removeClass("figureIconOn");
+$(document).ready(function() {
+    // Otevření submenu při kliknutí na košík
+    $('#shopCartIcon').click(function(event) {
+        event.stopPropagation(); // Zabraňuje propagačnímu efektu kliknutí na dokument
+        $('#submenu').toggle(); // Přepíná zobrazení submenu
+    });
+
+    // Zavření submenu při kliknutí mimo submenu nebo ikonu
+    $(document).click(function(event) {
+        if (!$(event.target).closest('#shopCartIcon, #submenu').length) {
+            $('#submenu').hide(); // Skrytí submenu
         }
     });
 
-    // Kliknutí kdekoliv na stránce zavře submenu, pokud je otevřené
-    $(document).click(function () {
-        if ($("#submenu").css("display") == "block") {
-            $("#submenu").hide(); // Skryje submenu
-        }
-        $("#figureIcon").removeClass("figureIconOn");
+    // Zabrání zavření submenu při kliknutí přímo na něj
+    $('#submenu').click(function(event) {
+        event.stopPropagation();
     });
-
-    // Zamezí zavření submenu při kliknutí uvnitř něj
-    $("#submenu").click(function (event) {
-        event.stopPropagation(); // Zastaví propagaci události kliknutí
-    });
-
 });
